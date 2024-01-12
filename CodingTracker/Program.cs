10ae8c35d98ff186e45adad584fb393ce00118e7
mikemem44 <mikelmm1999@gmail.com>
2024-01-12 09:33:34 -0600

    Revert "Keeping one message"
    This reverts commit b29908750c938ebeafc8acd26cabba3de3ed4e47.

--- CodingTracker/Program.cs
+++ CodingTracker/Program.cs
@@ -1,2 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, C# Academy! One more modification!"); 
+Console.WriteLine("Hello, C# Academy! One more modification!"); 
+Console.WriteLine("Hello, C# Academy! One more modification!");
+Console.WriteLine("Hello, C# Academy! One more modification!");
 Console.WriteLine("Hello, C# Academy! One more modification!");
